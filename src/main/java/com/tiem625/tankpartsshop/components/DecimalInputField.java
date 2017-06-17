c4eb5d93966cbe36fd62bfc2772bc163c26225af
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2017-06-17 10:45:12 +0300

    trying to add custom components

---
+++ src/main/java/com/tiem625/tankpartsshop/components/DecimalInputField.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tiem625.tankpartsshop.components;
+
+import java.math.BigDecimal;
+import javafx.scene.control.TextField;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author Tiem625
+ */
+public class DecimalInputField extends TextField {
+    
+    private final String DECIMAL_REGEX = "/^\\d+\\.?\\d*$/";
+    
+    public DecimalInputField() {
+        super();
+        this.textProperty().addListener((value, oldVal, newVal) -> {
+            if (!newVal.matches(DECIMAL_REGEX)) {
+                this.setText(newVal.replaceAll("\\D+", ""));
+            }
+        });
+    }
+    
+    public DecimalInputField(BigDecimal value) {
+        this();
+        setValue(value);
+    }
+   
+    public BigDecimal getValue() {
+        if (!StringUtils.isEmpty(getText())) {
+            return new BigDecimal(getText());
+        } else {
+            return BigDecimal.ZERO;
+        }
+    }
+    
+    public void setValue(BigDecimal value) {
+        setText(value != null? value.toString(): "");
+    }
+    
+}
